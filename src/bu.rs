694f8bdacc5e6a7d848384496a224889baf7055c
ACR1209 <andrescoronel1209@gmail.com>
2023-06-10 00:22:21 -0500

    Made code more rust like

--- src/bu.rs
+++ src/bu.rs
@@ -3,11 +3,16 @@ pub struct BitUtils {}
 impl BitUtils {
     // Transforms a decimal represented byte into its bit representation
     pub fn byte_to_bit(byte: u8) -> Vec<u8> {
+        (0..8).rev().map(|i| (byte >> i) & 1).collect()
+    }
+
+    // Transforms a decimal represented 4 byte into its bits representation
+    pub fn byte_u32_to_bit(byte: u32) -> Vec<u8> {
         let mut bits: Vec<u8> = Vec::new();
 
-        for i in (0..8).rev() {
+        for i in (0..32).rev() {
             let bit = (byte >> i) & 1;
-            bits.push(bit);
+            bits.push(bit as u8);
         }
 
         bits
@@ -26,15 +31,20 @@ impl BitUtils {
         output
     }
 
+    // Transforms 4 bytes in its bit form into its decimal representation
+    pub fn byte_u32_to_decimal(byte: Vec<u8>) -> u32 {
+        byte.iter()
+            .enumerate()
+            .filter(|(_, &bit)| bit == 1)
+            .fold(0u32, |acc, (i, _)| acc + 2u32.pow(31 - i as u32))
+    }
+
     // Reads the least significant bit (LSB) from a byte array
     pub fn read_lsb(bytes: Vec<u8>) -> Vec<u8> {
-        let mut output: Vec<u8> = Vec::new();
-
-        for byte in bytes.iter() {
-            output.push(byte % 2);
-        }
-
-        output
+        bytes
+            .iter()
+            .map(|byte| byte % 2)
+            .collect()
     }
 
     // Takes bits and transforms them into bytes
@@ -52,12 +62,9 @@ impl BitUtils {
 
     // Takes bytes and transforms them into a bit array
     pub fn make_bits(bytes: Vec<u8>) -> Vec<u8> {
-        let mut bits: Vec<u8> = Vec::new();
-
-        for byte in bytes.iter() {
-            bits.append(&mut Self::byte_to_bit(*byte));
-        }
-
-        bits
+        bytes
+            .iter()
+            .flat_map(|byte| Self::byte_to_bit(*byte))
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,47 @@ use std::{
 use png::{Decoder, Encoder};
 mod bu;
 use bu::BitUtils;
+use clap::Parser;
+
+
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        println!("[INFO] {}", format_args!($($arg)*));
+    }};
+}
+macro_rules! success {
+    ($($arg:tt)*) => {{
+        println!("[SUCCESS] {}", format_args!($($arg)*));
+    }};
+}
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        eprintln!("[ERROR] {}", format_args!($($arg)*));
+    }};
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// If is to read or to hide
+    #[arg(short, long)]
+    option: String,
+
+    /// The path to the file to read
+    #[arg(short, long)]
+    file: Option<String>,
+
+    /// The path to the image to use
+    #[arg(short, long)]
+    image: String,
+
+    /// The path to output the file
+    #[arg(long)]
+    output: String,
+}
 
 fn decode(src: &str, dest: &str) {
+    info!("Getting image data");
     let decoder = Decoder::new(File::open(src).unwrap());
 
     let mut binding = decoder.read_info();
@@ -16,34 +55,42 @@ fn decode(src: &str, dest: &str) {
     let mut data = vec![0; reader.output_buffer_size()];
     reader.next_frame(&mut data).unwrap();
 
-    let (header, remainder) = data.split_at(8);
-    let message_len = BitUtils::byte_to_decimal(BitUtils::read_lsb(header.to_vec()));
+    let (message_len, image_data) = data.split_at(32);
+    let message_len = BitUtils::byte_u32_to_decimal(BitUtils::read_lsb(message_len.to_vec()));
+    info!("Message size of {} bits", &message_len);
+    let (bytes_message, _): (&[u8], &[u8]) = image_data.split_at(message_len as usize);
+    let message_bits = BitUtils::read_lsb(bytes_message.to_vec());
 
-    let (message_bytes, _) = remainder.split_at(message_len as usize);
-    let message_retrived = BitUtils::read_lsb(message_bytes.to_vec());
-
-    let message = BitUtils::bits_to_bytes(message_retrived);
+    let message_retrived = BitUtils::bits_to_bytes(message_bits);
 
     let mut output_file = File::create(Path::new(dest)).unwrap();
-    output_file.write_all(&message).unwrap();
+    
+    info!("Writing message found to file {}", &dest);
+    output_file.write_all(&message_retrived).unwrap();
+
+    success!("Succesfully retrived message to {}", &dest);
 }
 
 fn encode(src: &str, msg_src: &str, dest: &str) {
+    info!("Transforming message to bytes");
     let message_bytes = read(msg_src).unwrap();
     let message_bits = BitUtils::make_bits(message_bytes);
-    let message_size = BitUtils::byte_to_bit(message_bits.len() as u8);
-
+    let message_size = BitUtils::byte_u32_to_bit(message_bits.len() as u32);
+    info!("Message size {} bits", message_bits.len());
+    info!("Embedding message size to message header");
     let mut complete_message = Vec::new();
     complete_message.extend_from_slice(&message_size);
     complete_message.extend_from_slice(&message_bits);
 
+    info!("Opening image {}", &src);
     let decoder = Decoder::new(File::open(src).unwrap());
 
     let mut binding = decoder.read_info();
     let reader = binding.as_mut().unwrap();
 
+    info!("Image capacity: {}", reader.output_buffer_size());
     if complete_message.len() > reader.output_buffer_size() {
-        eprintln!("Image is too small!");
+        error!("Image is too small: message size is {} and image allows for {}", complete_message.len(), reader.output_buffer_size());
         return;
     }
 
@@ -52,6 +99,7 @@ fn encode(src: &str, msg_src: &str, dest: &str) {
 
     let info = reader.info();
 
+    info!("Saving information in the LSB");
     let mut i = 0;
     for bit in complete_message.iter() {
         if *bit == 1 && data[i] % 2 == 0 {
@@ -68,19 +116,32 @@ fn encode(src: &str, msg_src: &str, dest: &str) {
 
     image_encoder.set_color(info.color_type);
     image_encoder.set_depth(info.bit_depth);
-
+    info!("Saving generated image to {}", &dest);
     image_encoder
         .write_header()
         .unwrap()
         .write_image_data(&data)
         .unwrap();
+    success!("Succesfully saved message on image {}", &dest);
 }
 
 fn main() {
-    let _ = encode(
-        "test.png",
-        "aea.txt",
-        "test_out.png",
-    );
-    let _ = decode("test_out.png", "text.txt");
+    let args = Cli::parse();
+
+    match args.option.as_str() {
+        "read"=>{
+            info!("Starting to read file {}", &args.image);
+            decode(&args.image, &args.output)
+        },
+        "write"=>{
+            match args.file {
+                Some(file)=> {
+                    info!("Starting to write file {}", &args.output);
+                    encode(&args.image, &file, &args.output)
+                },
+                None=>eprintln!("ERROR: File not passed!")
+            }
+        },
+        _ => panic!("No valid option given: please try to use --help to see the valid options"),
+    }
 }
